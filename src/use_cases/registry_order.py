0f05bef6259fbf6be0238be376a2e95034fc32e3
Marcello Magno <marcello_magno@hotmail.com>
2025-04-09 11:12:44 -0300

    adding Validator in registry_order

--- src/use_cases/registry_order.py
+++ src/use_cases/registry_order.py
@@ -2,15 +2,16 @@ from datetime import datetime
 from src.models.repository.interfaces.orders_repository import OrdersRepositoryInterface
 from src.main.http_types.http_request import HttpRequest
 from src.main.http_types.http_response import HttpResponse
-
+from src.validators.registry_order_validator import registry_order_validator
 
 class RegistryOrder:
     def __init__(self, orders_repository: OrdersRepositoryInterface) -> None:
         self.__orders_repository = orders_repository
 
-    def registry(self, http_request):
+    def registry(self, http_request: HttpRequest) -> HttpResponse:
         try:
             body = http_request.body
+            self.__validator_body(body)
 
             new_order = self.__format_new_order(body)
             self.__registry_order(new_order)
@@ -23,6 +24,10 @@ class RegistryOrder:
                 },
                 status_code=400
             )
+        
+    def __validator_body(self, body: dict) -> None:
+        registry_order_validator(body)
+
     def __format_new_order(self, body: dict) -> dict:
         new_order = body["data"]
         new_order = {**new_order, "created_at": datetime.now()}
